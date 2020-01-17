b89eed758768389363378eb65d67941e09d1d526
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-01-16 23:56:10 -0300

    User dev registered with all data from github

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,25 @@
 const { Router } = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
-routes.post('/users', (request, response) => {
-	return response.json({ message: 'Who is coding?'});
+routes.post('/devs', async (request, response) => {
+	const { github_username, techs } = request.body;
+	const apiresponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+	const { name = login, avatar_url, bio } = apiresponse.data;
+
+	const techsArray = techs.split(',').map(tech => tech.trim());
+
+	const dev = await Dev.create({
+		github_username,
+		name,
+		avatar_url,
+		bio,
+		techs: techsArray,
+	})
+	return response.json(dev);
 });
 
 module.exports = routes;
