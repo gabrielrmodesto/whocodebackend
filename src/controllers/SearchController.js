f33c6a4b0347c1842ed86c732d7e58f19b72a45e
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-01-17 00:56:41 -0300

    Backend finish

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -6,6 +6,22 @@ module.exports = {
 		const { latitude, longitude, techs } = request.query;
 
 		const techsArray = parseStringAsArray(techs);
-		
+
+		const devs = Dev.find({
+			techs: {
+				$in: techsArray,
+			},
+			location: {
+				$near: {
+					$geometry: {
+						type: 'Point',
+						coordinates: [longitude,latitude],
+					},
+					$maxDistance: 10000,
+				},
+			},
+		});
+
+		return response.json(devs);
 	}
 }
