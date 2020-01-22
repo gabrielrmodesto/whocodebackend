de89acfc92df63b029fd6048fdfd2667522499f0
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-01-21 22:40:06 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 
-mongoose.connect('mongodb+srv://gabriel:1p3p4s5s6a7a@whocode-wy8ik.mongodb.net/whocode?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://<username>:<password>@whocode-wy8ik.mongodb.net/whocode?retryWrites=true&w=majority', {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
